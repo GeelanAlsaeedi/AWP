912d39fb15ba9756cfe694fd2b36dea5a3e7a566
0r-a-n <gh.seyami@gmail.com>
2021-04-14 14:36:43 +0300

    Update index.php

--- ConnectionPages/index.php
+++ ConnectionPages/index.php
@@ -97,36 +97,35 @@
           <?php
           if (isset($_POST['sign_in'])){
 
-            if((!empty($_POST['userName'])) && (!empty($_POST['password']))){
-              $userName=$_POST['userName'];
-              $password=$_POST['password'];
+			 if((!empty($_POST['userName'])) && (!empty($_POST['password']))){
+				  $userName=$_POST['userName'];
+				  $password=$_POST['password'];
 
-              // To protect MySQL injection (more detail about MySQL injection)
-              $userName = stripslashes($userName);
-              $password = stripslashes($password);
+				  // To protect MySQL injection (more detail about MySQL injection)
+				  $userName = stripslashes($userName);
+				  $password = stripslashes($password);
 
-              $query="SELECT * FROM artist_info WHERE user_name='$userName' AND password='$password'";
+				  $query="SELECT * FROM artist_info WHERE user_name='$userName' AND password='$password'";
 
-              if ($r=mysqli_query($dbc, $query)){
-                // Retrieve and print every record:
-                //print '<p>Your loged in successfaully!</p>';
-                while ($row = mysqli_fetch_array($r)) {
+				  if ($r=mysqli_query($dbc, $query)){
+					// Retrieve and print every record:
+					while ($row = mysqli_fetch_array($r)) {
 
-                 //print"<p><h3>{$row['user_name']}</h3></p>";
-                  session_start();
-                  $_SESSION['loggedin'] = true;
-                  $_SESSION['userName'] = $userName;
+					 //print"<p><h3>{$row['user_name']}</h3></p>";
+					  session_start();
+					  $_SESSION['loggedin'] = true;
+					  $_SESSION['userName'] = $userName;
 
-                  header('Location: ProfilePage.php');
-                  exit();
-                }
-              }
-               print '<p>User name or password incorrect</p>';
-
-            }else{
-              print'<p> Please fill all the required fieleds</p>';
+					  header('Location: ProfilePage.php');
+					  exit();
+					}
+				  }
+				   print '<p>User name or password incorrect</p>';
+				   
+			    }else{
+			  print'<p> Please fill all the required fieleds</p>';
+			 }
             }
-          }
         ?>
         </div>
         </form>
